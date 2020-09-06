9805ebf12f1a4dad3b23b94974852fc81771b4fa
c611701142 <km9216bp@gmail.com>
2020-09-06 11:44:18 +0900

    関数の添削をお願いします。

--- practice.hpp
+++ practice.hpp
@@ -17,7 +17,7 @@ public:
     static constexpr uint8_t kLeafChar = '\0';
     static constexpr uint8_t MaxUint8_t = 0xFF;
     static constexpr int kEmptyCheck = 0xFFFFFFFF;
-    static constexpr int kEmptyBase = 0xFFFFFFFF;
+    static constexpr int kEmptyBase = 0xFFFFFFFF;//4294967295 int型の最大値 16^8-1
     static constexpr int kFailedIndex = 0;
 private:
     struct Unit {
@@ -37,79 +37,113 @@ public:
         int node = 0;
         int n = 0;
         for(uint8_t c : str) {//各文字に１　数字を割り当て
-            int next_node = Transition(node, c);
-            std::unordered_map<uint8_t, int> tmp_base; // base値を保存する
-            std::unordered_map<uint8_t, int> tmp_index; // indexを保存する
-            std::vector<std::unordered_map<int, uint8_t>> tmp_child; // 子の子を保存する
-            if (next_node == kFailedIndex) {
-                auto row = GetChildren(node); // 子を取得
-                //std::cout << "----------------------------------------------------" << std::endl;
-                //std::cout <<  str << std::endl;
-                std::cout << "node : " << node << std::endl;
-                //std::cout << "n : " << n << std::endl;
-                //uint8_t v = str[n];
-                //std::cout << "v: " << v << ", " << int(v) << std::endl;
-                //std::cout << "bc_size : " << bc_.size() << std::endl;
-                //std::cout << "next_node : " << next_node << std::endl;
-                //std::cout << "row[0] : " << row[0] << std::endl;
-                
-                if (row.size() == 0) { // 子が存在しないとき
-                 //   std::cout << "not child" << std::endl;
-                    InsertSuffix(node, std::string_view(str).substr(n));
-                    break;
-                }
-                else { // 子が存在するとき
-                    int next_index = bc_[node].base + c;
-                    if(bc_[next_index].check == kEmptyCheck) {
-                        bc_[next_index].check = node;
-                    //    std::cout << "children exists in space" << std::endl;
-                        InsertSuffix(next_index, std::string_view(str).substr(n+1));
-                        break;
-                    }
-                    else {
-                        // いろいろな値の保存
-                        for(int i=0; i < row.size(); i++) {
-                            uint8_t a = row[i];
-                        //    std::cout << "row_value : " << a << ", " << int(a) << std::endl;
-                            int index = bc_[node].base + a;
-                            tmp_base[a] = bc_[index].base;
-                            tmp_index[a] = index;
-
-                            // 子の子の値の保存
-                            bool flag = false;
-                            for(int i = 0; i < tmp_child.size(); i++) {
-                                if(tmp_child[i][index]) {
-                                    flag = true;
-                                }
-                            }
-                            if(flag == false) {
-                                for(int i=0; i < MaxUint8_t; i++) {
-                                    uint8_t b = i;
-                                    if(bc_[bc_[index].base + i].check == index) {
-                                        tmp_child.emplace_back();
-                                        tmp_child[tmp_child.size()-1][index] = i;
-                                    }
-                                }
-                            }
-
-                            // 使わない部部の消去
-                            bc_[index].base = kEmptyBase;
-                            bc_[index].check = kEmptyCheck;
-                        }
-                        //std::cout << "children exists not space" << std::endl;
-                        ModifyAndInsertSuffix(node, tmp_child, tmp_base, tmp_index, row, c, std::string_view(str).substr(n+1));
-                        tmp_base.clear();
-                        tmp_index.clear();
-                        tmp_child.clear();
-                        break;
-                    }
-                }
-            }
-            node = next_node;
-            n++;
+           
         }
     }
 
+private:
+    //base[index]にvalを格納する
+     //valは設定する値
+     //index > DA_SIZEの時のDA_SIZEをindexまで拡張する
+    void w_check(int index,int val){
+        bc_[index].check = val;
+        if(index > bc_.size()){
+            bc_.resize(index);
+        }
+     }
+
+    //9/4
+    //新しいbase値の決定
+     int x_check(int a){
+        //D-1　初期化
+        int q = 1;bool flag = true;
+        uint8_t c;
+        //D-3  終了判定
+        if(q <= bc_.size() ){
+            flag = true;
+        }
+        else if(q > bc_.size()){
+            flag = false;
+        }
+        //D-2 インデックス検索  flag = trueで入る
+        if(flag == true){
+            if(bc_[q + c].check == 0){
+                return q;
+            }
+            else{
+                q++;  
+            }
+        }
+        else if(flag == false){
+            return q;
+            exit(0);
+        }
+    }
+
+    void w_base(int index,int val){
+        bc_[index].base = val;
+        if(index > bc_.size()){
+            bc_.resize(index);
+        }
+
+
+    }
+
+public:
+    //キー追加時に衝突が起こった場合、ノードの移動によって衝突を回避し、
+    //キーの残りの文字列を追加する
+    void insert2(int index,int pos){
+        std::string str;
+        std::vector<uint8_t> row;
+        uint8_t c;
+        std::vector<uint8_t> a;
+        //衝突が起こったときの処理 I-1
+        //for(uint8_t c : str) {//各文字に１　数字を割り当て
+            int idx = bc_[index].base + c;
+            if(bc_[idx].check > 0){
+                row = getlabel(index);//変数Rにセット
+            //}
+        //遷移先の定義 I-2
+        int t = bc_[index].base + c;
+        //bc_[index].check = w_check(t,index);遷移を定義？
+        pos++;
+        //残りの文字列の追加 I-3
+        //a_posとは何か？
+        int new_base = x_check(a[pos]);//新しいbase値を定義
+        t = bc_[index].base + a[pos];
+        bc_[t].check = index;
+        pos++;
+
+        //まだ追記あり　２３２２
+        }
+    }
+
+    void modify(int index,std::vector<uint8_t> row,int b){
+        uint8_t c;
+        int q = 0;
+        //M-1新しいbase[index]の決定
+        int old_base = bc_[index].base;//退避しておく
+        //bc_[index].base = w_check(index,b);//新しいbase値を設定
+        //M-2遷移の移動
+        int old_t = old_base + c;
+        int t =  bc_[index].base + c;
+        w_base(t,bc_[old_t].base);
+        if(bc_[old_t].base > 0){
+            if(bc_[q].check == old_t){
+                //bc_[q].check = w_check(q,t);
+            }
+        } //新しい遷移先の遷移先の再定義が必要になる M-3
+         //check[q] = old_base　になるすべてのq にたいしてw_check(q,t)により、checkを再定義
+        
+    }
+    
+    //葉ノードindexを削除することで実現
+    //検索成功時に呼び出す
+    void delete_sucsess(int index){
+        w_base(index,0);
+        w_check(index,0); 
+    }
+//単語
     bool contains(const std::string& key) const {
         int node = 0; // root
         //std::cout << "--------------contains---------------" << std::endl;
@@ -117,7 +151,7 @@ public:
         for (uint8_t c : key) {//一文字ずつループを回す
             //std::cout << "uint8_t : " << c << std::endl;
             //std::cout << "node  : " << node << std::endl;
-            int next_node = Transition(node, c);
+            int next_node = forward(node, c);
            // std::cout << "check : " << bc_[next_node].check << std::endl;
             if (next_node == kFailedIndex) {
                 //std::cout << "-------Fauiled value check------------" << std::endl;
@@ -140,23 +174,9 @@ public:
             node = next_node;
         }
         // '\0'
-        int next_node = Transition(node, kLeafChar);
-        std::cout << "--------kLeafer check---------------------" << std::endl;
-        std::cout << "key : " << key << std::endl;
-        std::cout << "node  : " << node << std::endl;
-        std::cout << "next_node : " << next_node << std::endl;
-        std::cout << "check : " << bc_[next_node].check << std::endl;
+        int next_node = forward(node, kLeafChar);
         return next_node != kFailedIndex;
     }
-    /*
-    void check() {
-        std::cout << "------bc_size : " << bc_.size() << "---------" << std::endl;
-        for(int i = 0; i < bc_.size(); i++) {
-            //std::cout << "i : " << i << std::endl;
-            std::cout << "check : " << bc_[i].check << std::endl;
-        }
-    }
-    */
 private:
     int find_base(const std::unordered_map<uint8_t, int>& row) const {
         for (int base = 0; true; ++base) {
@@ -174,6 +194,7 @@ private:
                 return base;
             }
         }
+        std::cout << "DA_SIZE() : " << bc_.size() << std::endl;
         return bc_.size();
     }
 
@@ -187,108 +208,8 @@ private:
         //std::cout << "index : " << index << std::endl;
     }
 
-    // 残りの文字列を新たに格納
-    void InsertSuffix(int r, std::string_view ax) {
-        std::unordered_map<uint8_t, int> row;
-        int node = r;
-        for (int i =0; i < ax.size(); i++) {
-            uint8_t c = ax[i];
-            row[c] = 0;
-            int base = find_base(row);
-            bc_[node].base = base;
-            int next_index = base + c;
-            expand(next_index);
-            bc_[next_index].check = node;
-            node = next_index;
-            row.clear();
-        }
-        // kLeafChar
-        row[kLeafChar] = 0;
-        int base = find_base(row);
-        bc_[node].base = base;
-        int next_index = base + kLeafChar;
-        expand(next_index);
-        bc_[next_index].check = node;
-        row.clear();
-    }
-
-    // 更新
-    void ModifyAndInsertSuffix(int r, 
-        std::vector<std::unordered_map<int, uint8_t>> tmp_child,
-        const std::unordered_map<uint8_t, int>& tmp_base, 
-        const std::unordered_map<uint8_t, int>& tmp_index, 
-        const std::vector<uint8_t>& row, 
-        uint8_t c, 
-        std::string_view str) 
-        {
-        int node = r;
-        std::unordered_map<uint8_t, int> tmp_row;
-        for(int i = 0; i < row.size(); i++) {
-            tmp_row[row[i]] = 0;
-        }
-        tmp_row[c] = 0;
-        int base = find_base(tmp_row);
-        bc_[r].base = base;
-        for (int i = 0; i < row.size(); i++) {
-            uint8_t a = row[i];
-            int next_index = base + a;
-            expand(next_index);
-            bc_[next_index].check = r;
-            //assert(tmp_base.count(a) == 1);
-            if(tmp_base.count(a) == 1){
-                //exit(0);//プログラム異常終了
-            }
-            bc_[next_index].base = tmp_base.find(a)->second;
-            //std::cout << "tmp_base : " << bc_[next_index].base << std::endl;
-            //子の子のcheck値を付け替える
-            //assert(tmp_index.count(a) == 1);
-            if(tmp_index.count(a) == 1){
-                //exit(0);//プログラム異常終了
-            }
-            int index = tmp_index.find(a)->second;
-            //std::cout << "index : " << index << std::endl;
-            if(a != kLeafChar) {
-                //for(int i = 0; i < 26; i++) {
-                    //uint8_t b = 'a' + i;
-                //std::cout << "a : " << a << ", " << int(a) << std::endl;
-                for(int j=0; j < tmp_child.size(); j++) {
-                    if(tmp_child[j][index]) {
-                        int next_next_index = bc_[next_index].base + tmp_child[j][index];
-                        bc_[next_next_index].check = next_index;
-                    }
-                }
-                /*
-                for(int i = 0; i < MaxUint8_t; i++) {
-                    uint8_t b = i;
-                    int next_next_index = bc_[next_index].base + b;
-                    if (bc_[next_next_index].check == index) {
-                        std::cout << "b : " << b << ", " << int(b) << std::endl;
-                        std::cout << "next_next_index : " << next_next_index << std::endl;
-                        std::cout << "next_index : " << next_index << std::endl;
-                        bc_[next_next_index].check = next_index;
-                    }
-                }
-                */
-            }
-            if (a != kLeafChar) {
-                int next_next_index = bc_[next_index].base + kLeafChar;
-                if (bc_[next_next_index].check == index) {
-                    bc_[next_next_index].check = next_index;
-                }
-            }
-        }
-        // uint8_t c の追加
-        if (c != kLeafChar) {
-            int next_index = base + c;
-            expand(next_index);
-            bc_[next_index].check = node;
-            node = next_index;
-            InsertSuffix(node, str);
-        }
-    }
-
-    // 親番号に対する子の集合を返すための関数
-    std::vector<uint8_t> GetChildren(int r) {
+     //ノードindexからでる遷移ラベルを要素とする集合Rを返す
+    std::vector<uint8_t> getlabel(int r) {
         std::vector<uint8_t> row;
         int count = 0;
         //std::cout << "base : " << bc_[r].base << std::endl;
@@ -320,17 +241,22 @@ private:
 
         return row;
     }
-
-    // 遷移が成功するかどうかの判定(成功する場合は次の親番号を返値, 失敗した場合は0を返値)
-    int Transition(int r, uint8_t c) const {
-        auto base = bc_[r].base;
-        
-        if (base == kEmptyBase) {
-            return kFailedIndex;
+    //8.30~黒田記述 
+    //transitionを確認するための関数
+    int forward(int r,uint8_t c)const{
+        int t = bc_[r].base + c;
+        if(0 < t && t < bc_.size() + 1){
+            if(bc_[t].check == r){
+                return t;
+            }
         }
-        int t = base + c;
-        return t < bc_.size() && bc_[t].check == r ? t : kFailedIndex;
-    }
+        else{
+            return kFailedIndex;//遷移失敗0を変えす
+        }
+        return t;//警告けすため　
+    }// control reaches end of non-void function
+
+   
 
 };
 
